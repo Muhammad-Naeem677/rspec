bdd42f0b6c395398b2827c4f2d8cf10ea809156a
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-06-23 20:16:43 +0500

    Rspec test using describe and it

--- card.rb
+++ card.rb
@@ -26,4 +26,31 @@ RSpec.describe 'a playing card' do
   it 'has a rank' do
     raise unless Card.new(suit: :spades, rank: 12).rank == 12
   end
+
+  describe 'a jack' do
+    it 'ranks higher than a 10' do
+      lower = Card.new(suit: :spades, rank: 10)
+      higher = Card.new(suit: :sapdes, rank: :jack)
+
+      raise unless higher.rank > lower.rank
+    end
+  end
+
+  describe 'a queen' do
+    it 'ranks higher than a jack' do
+      lower = Card.new(suit: :spades, rank: :jack)
+      higher = Card.new(suit: :sapdes, rank: :queen)
+
+      raise unless higher.rank > lower.rank
+    end
+  end
+
+  describe 'a king' do
+    it 'ranks higher than a queen' do
+      lower = Card.new(suit: :spades, rank: :queen)
+      higher = Card.new(suit: :sapdes, rank: :king)
+
+      raise unless higher.rank > lower.rank
+    end
+  end
 end
