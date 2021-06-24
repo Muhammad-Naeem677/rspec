0c2c535e2811dc03aab7fa559c50550af6fdc792
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-06-24 18:15:14 +0500

    Each 'it' has different object_id

--- array_spec.rb
+++ array_spec.rb
@@ -8,13 +8,14 @@ RSpec.describe 'an array' do
     puts
 
     it 'has a length' do
-        puts self
-        puts self.class
+        puts self.object_id
         puts
         raise unless build_array("a").length == 1
     end
 
     it 'has a first element' do
+        puts self.object_id
+        puts
         raise unless build_array("a").first == "a"
     end
 
