f006cbc63b1494eedfde8440a5644c78582f944b
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-06-25 16:33:11 +0500

    Getting all not contiguous values not the first one

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,20 +1,19 @@
 require 'deck'
 
-RSpec::Matchers.define(:be_contiguous_by_attribute) do |attr|
-    puts "attributes=#{attr}"
+RSpec::Matchers.define(:be_contiguous_by) do
     match do |array|
-        !first_non_contiguous_pair(array, attr)
+        !first_non_contiguous_pair(array)
     end
 
     failure_message do |array|
-        "%s and %s were not contiguous" % first_non_contiguous_pair(array, attr)
+        "%s and %s were not contiguous" % first_non_contiguous_pair(array)
     end
 
-    def first_non_contiguous_pair(array, attr)
+    def first_non_contiguous_pair(array)
         array
-            .sort_by { |x| x.send(attr) }
+            .sort_by(&block_arg)
             .each_cons(2)
-            .detect { |x, y| x.send(attr) + 1 != y.send(attr) }
+            .detect { |x, y| block_arg.call(x) + 1 != block_arg.call(y) }
     end
 end
 
@@ -25,9 +24,8 @@ describe 'Deck' do
         end
 
         it 'has contiguous ranks by suit' do
-            Deck.all.group_by { |card| card.suit }.each do |suit, cards|
-                expect(cards).to be_contiguous_by_attribute('rank')
-            end
+            expect(Deck.all.group_by { |card| card.suit}.values).to all(be_contiguous_by(&:rank))
+            # end
         end
     end
 end
