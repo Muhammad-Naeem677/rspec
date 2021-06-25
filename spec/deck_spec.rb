3921699cc9cb5aea1765ef3596c880cd4f392dcf
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-06-25 12:32:54 +0500

    All tests together

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,6 +7,14 @@ describe 'Deck' do
         end
 
         it 'has seven as it lowest card' do
+            # Deck::SUITS.each do |suit|
+            #     expect(Deck.all).to include(Card.build(suit, 8))
+            #     expect(Deck.all).to_not include(Card.build(suit, 7))
+            # end
+            # Deck.all.each do |card|
+            #     expect(card.rank).to be >= 8                
+            # end
+            # expect(Deck.all.map { |card| card.rank }).to all(be >= 8)
             expect(Deck.all).to all(have_attributes(rank: be >= 8))
         end
     end
