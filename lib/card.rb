6d26f200b0d4eb80a49795d623df41168d473182
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-06-25 12:21:15 +0500

    Making good structure

--- lib/card.rb
+++ lib/card.rb
@@ -16,7 +16,7 @@ class Card
   end
 
   def inspect
-    "<Card #{rank} #{suit}>"
+    to_s
   end
 
   def suit
@@ -38,5 +38,28 @@ class Card
   def eql?(other)
     self == other
   end
+
+  def to_s
+    id = if rank > 10
+      {
+        11 => "J",
+        12 => "Q",
+        13 => "K",
+        14 => "A"
+      }.fetch(rank)
+    else
+      rank.to_s
+    end
+
+    s = {
+      hearts: "♡ ",
+      spades: "♤ ",
+      diamonds: "♢ ",
+      clubs: "♧ ",
+    }
+
+    "#{id.upcase}#{s.fetch(suit)}"
+  end
+  
 end
 
