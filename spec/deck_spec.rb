234a7b0a7eb7e05e389548422f7bc857c37b9e14
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-06-25 16:41:26 +0500

    --Generic code-- Cusotm matchers in a module

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,23 +1,28 @@
 require 'deck'
 
-RSpec::Matchers.define(:be_contiguous_by) do
-    match do |array|
-        !first_non_contiguous_pair(array)
-    end
+module ArrayMatchers
+    extend RSpec::Matchers::DSL
 
-    failure_message do |array|
-        "%s and %s were not contiguous" % first_non_contiguous_pair(array)
-    end
+    matcher :be_contiguous_by do
+        match do |array|
+            !first_non_contiguous_pair(array)
+        end
 
-    def first_non_contiguous_pair(array)
-        array
-            .sort_by(&block_arg)
-            .each_cons(2)
-            .detect { |x, y| block_arg.call(x) + 1 != block_arg.call(y) }
+        failure_message do |array|
+            "%s and %s were not contiguous" % first_non_contiguous_pair(array)
+        end
+
+        def first_non_contiguous_pair(array)
+            array
+                .sort_by(&block_arg)
+                .each_cons(2)
+                .detect { |x, y| block_arg.call(x) + 1 != block_arg.call(y) }
+        end
     end
 end
 
 describe 'Deck' do
+    include ArrayMatchers
     describe '.all' do
         it 'contains 32 cards' do
             expect(Deck.all.length).to eq(32)
