cda22b241a785f32c3351e635d3d8a2fdb89ae5a
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-06-25 13:16:17 +0500

    checking contiguous ranks by suit

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,16 +6,16 @@ describe 'Deck' do
             expect(Deck.all.length).to eq(32)
         end
 
-        it 'has seven as it lowest card' do
-            # Deck::SUITS.each do |suit|
-            #     expect(Deck.all).to include(Card.build(suit, 8))
-            #     expect(Deck.all).to_not include(Card.build(suit, 7))
-            # end
-            # Deck.all.each do |card|
-            #     expect(card.rank).to be >= 8                
-            # end
-            # expect(Deck.all.map { |card| card.rank }).to all(be >= 8)
-            expect(Deck.all).to all(have_attributes(rank: be >= 8))
+        it 'has contiguous ranks by suit' do
+            Deck.all.group_by { |card| card.suit }.each do |suit, cards|
+                contiguous = cards
+                    .map { |card| card.rank }
+                    .sort
+                    .each_cons(2)
+                    .all? { |x, y| x + 1 == y }
+                
+                expect(contiguous).to eq(true)
+            end
         end
     end
 end
