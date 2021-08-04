b1e751534ff4765a1793225f961abeb157a34904
Muhammad Naeem <muhammad.naeem@cognitiveaxis.com>
2021-08-04 13:19:35 +0500

    added new method and test it by parsing numbers

--- lib/card.rb
+++ lib/card.rb
@@ -60,6 +60,29 @@ class Card
 
     "#{id.upcase}#{s.fetch(suit)}"
   end
+
+  def self.from_string(value)
+    # Get final character in string
+    short_suit = value[-1]
+
+    # Map it to a suit
+    suit = {
+      "H" => :hearts,
+      "D" => :diamonds,
+      "S" => :spades,
+      "C" => :clubs,
+    }.fetch(short_suit)
+
+    # Map remainder to a face card, or fallback to numeric
+    rank = {
+      'A' => :ace,
+      'K' => :king,
+      'Q' => :queen,
+      'J' => :jack,
+    }.fetch(value[0]) { value[0..-2].to_i }
+
+    Card.build(suit, rank)
+  end
   
 end
 
